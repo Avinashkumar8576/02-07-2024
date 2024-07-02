90ca3bf8161115652a4a6ed287fcd04709d0da7e
Avinashkumar8576 <avinashkumar90089008@gmail.com>
2024-07-02 22:02:59 +0530

    first commit

---
+++ src/javaMethods/Example2.java
@@ -0,0 +1,19 @@
+package javaMethods;
+
+public class Example2 
+{
+	
+	
+	/**
+	 * 
+	 * main(): the java application/program execution starts from main() method.
+	 */
+	//main main()
+	public static void main(String[] args) 
+	{	
+		//2 instructions
+		String name = "Dinesh Kumar";//variable definition	
+		System.out.println(name);//print statement
+	}
+
+}
